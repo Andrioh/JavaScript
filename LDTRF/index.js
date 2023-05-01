99a736aaeab428c778e0238212af8479acf14a0e
Andrioh <121702052+Andrioh@users.noreply.github.com>
2023-05-01 06:24:44 -0300

    .

--- LDTRF/index.js
+++ LDTRF/index.js
@@ -1,3 +1,4 @@
+let Contador = 0
 let input = document.getElementById("inputTarefa");
 let btnAdd = document.getElementById("btn-add");
 let main = document.getElementById("areaLista");
@@ -7,15 +8,19 @@ function addTarefa() {
     let valorInput = input.value;
 
     if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        let novoItem = `<div class="item">
+      
+        
+        ++Contador;
+      
+        let novoItem = `<div id="${Contador}" class="item">
         <div class="item-icone">
-            <img src="Circulo.png" alt="Botão de marcação" class="Icone">
+            <img onclick="marcarTarefa(${Contador})" src="Circulo.png" alt="Botão de marcação" class="Icone">
         </div>
-        <div class="item-nome">
+        <div onclick="marcarTarefa(${Contador})" class="item-nome">
             ${valorInput}
         </div>
         <div class="item-botao">
-            <button class="delete">Deletar</button>
+            <button onclick="deletar(${Contador})" class="delete">Deletar</button>
         </div>
     </div>`;
 
@@ -26,3 +31,28 @@ function addTarefa() {
     }
 
 }
+
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if(classe=="item"){
+        item.classList.add("clicado");
+
+        item.parentNode.appendChild(item);
+    }else{
+        item.classList.remove("clicado");
+    }
+}
+
+input.addEventListener("keyup", function(event){
+    if (event.keyCode === 13){
+        event.preventDefault();
+        btnAdd.click()
+    }
+})
